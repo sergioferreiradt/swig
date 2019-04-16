6f8a3c6f407c669a897ccff726573f3282ec0bfd
Vadim Zeitlin <vz-swig@zeitlins.org>
2019-04-17 00:52:43 +0200

    Update auto-generated files for MSVC build for 4.0.0-beta1
    Update SWIG version and add yet another the new warning.

--- Source/Include/msvc/swigconfig.h
+++ Source/Include/msvc/swigconfig.h
@@ -58,13 +58,13 @@
 #define PACKAGE_NAME "swig"
 
 /* Define to the full name and version of this package. */
-#define PACKAGE_STRING "swig 3.0.12-1629-g85c5eb6a0"
+#define PACKAGE_STRING "swig 4.0.0-beta1-26-g6221b8676"
 
 /* Define to the one symbol short name of this package. */
 #define PACKAGE_TARNAME "swig"
 
 /* Define to the version of this package. */
-#define PACKAGE_VERSION "3.0.12-1629-g85c5eb6a0"
+#define PACKAGE_VERSION "4.0.0-beta1-26-g6221b8676"
 
 /* Define to 1 if you have the ANSI C header files. */
 #define STDC_HEADERS 1
@@ -86,7 +86,7 @@
 #define SWIG_PLATFORM "Win32"
 
 /* Version number of package */
-#define VERSION "3.0.12-1629-g85c5eb6a0"
+#define VERSION "4.0.0-beta1-26-g6221b8676"
 
 
 /* Default language */
